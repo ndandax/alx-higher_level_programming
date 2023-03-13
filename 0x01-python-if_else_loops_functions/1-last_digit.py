440fb0aa430df5f35242e3482f350ecf037e2610
ndandax <sebandandala99@gmail.com>
2023-03-13 11:40:27 +0000

    d8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ y = str(number)
 if int(y[-1]) > 5:
     print("Last digit of " + y + " is " + y[-1] + " and is greater than 5")
 elif int(y[-1]) == 0:
-    print("Last digit of " + y + " is " + y[-1] \
+    print("Last digit of " + y + " is " + y[-1] 
 + " and is 0")
 elif int(y[-1]) < 6 and y[-1] != 0:
-    print("Last digit of " + y + " is " + y[-1] \
+    print("Last digit of " + y + " is " + y[-1]
 + " and is less than 6 and not 0")
