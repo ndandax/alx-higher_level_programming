7d91a688761027c1c46822e4499653e172bac742
ndandax <sebandandala99@gmail.com>
2023-03-15 10:02:24 +0000

    more9

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,6 @@
 def search_replace(my_list, search, replace):
     for i in range(len(my_list)):
         if my_list[i] == search:
-            my_list[i] = replace
+            my_list8[i] = replace
+            i++
     return (my_list)
