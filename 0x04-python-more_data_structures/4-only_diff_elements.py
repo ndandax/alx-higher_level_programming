f7c57cc374be1d3ccdfd47ad395aa1ddeede69cb
ndandax <sebandandala99@gmail.com>
2023-03-15 15:11:49 +0000

    5

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    def only_diff_elements(set_1, set_2):
-        return (set_1.difference(set_2))
+def only_diff_elements(set_1, set_2):
+    return (set_1 ^ set_2)
