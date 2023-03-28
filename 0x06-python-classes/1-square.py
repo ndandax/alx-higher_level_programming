a3db8fa0027a98cbc3d23fd53de22df5ac5dc251
ndandax <sebandandala99@gmail.com>
2023-03-28 14:23:12 +0000

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class with private name"""
+
+
+class Square:
+    """defining class with private attribute"""
+    def __init__(self):
+        """defining private attribute"""
+        self.__size = size
