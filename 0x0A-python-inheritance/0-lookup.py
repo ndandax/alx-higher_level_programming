472e8d5a0e489b4811ecf3e8dedb4789923fa73b
ndandax <sebandandala99@gmail.com>
2023-04-11 09:32:23 -0400

    inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class that defines an a fuctions that return list of available attributes"""
+
+
+def lookup(obj):
+    """function that returns the list of available attributes and methods of an
+    object"""
+
+    return dir(object)
