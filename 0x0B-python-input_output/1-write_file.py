c1967fcb10948cf0574815deaf8d4845b38bc4cb
ndandax <sebandandala99@gmail.com>
2023-04-12 12:57:54 -0400

    input

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""writing to a file"""
+
+
+def write_file(filename="", text=""):
+    """a fuction that writes text to a file"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(text)
