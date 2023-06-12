675fe8f5d121980d2d73009924abc84a95175abd
ndandax <sebandandala99@gmail.com>
2023-06-12 23:52:34 +0300

    javasript

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const arg = process.argv[2];
 
 if (!isNaN(arg)) {
-    console.log("My number: " + parseInt(arg));
+  console.log('My number: ' + parseInt(arg));
 } else {
-    console.log('Not a number');
+  console.log('Not a number');
 }
