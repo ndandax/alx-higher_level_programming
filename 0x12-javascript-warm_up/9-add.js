dd383734eea8a888388060bcabe6fb32f41203c2
ndandax <sebandandala99@gmail.com>
2023-06-13 17:26:42 +0300

    javascript_addition

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const a = process.argv[2];
+const b = process.argv[3];
+function add(a, b) {
+    return (parseInt(a) + parseInt(b));
+}
+const result = add(a, b);
+console.log(result);
