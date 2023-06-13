09789298f61c7daac370fbca0edd4a3b876ff7ba
ndandax <sebandandala99@gmail.com>
2023-06-14 00:04:35 +0300

    javascript

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
+
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
-    if (w === 0 || h === 0 || h < 0 || w < 0) {
-      const emptyObject = Object.create(null);
-      this.emptyObject = emptyObject;
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
 }
