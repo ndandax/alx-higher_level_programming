c547699be211e5349d3fb7f6044ca3b3e14b6c8f
ndandax <sebandandala99@gmail.com>
2023-06-14 10:32:40 +0300

    javascript

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    const a = this.width;
+    this.width = this.height;
+    this.height = a;
+  }
+
+  double () {
+    const d = this.width * 2;
+    const k = this.height * 2;
+    this.width = d;
+    this.height = k;
+  }
+}
+module.exports = Rectangle;
