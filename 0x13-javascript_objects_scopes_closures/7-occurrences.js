fd09bf1bd87d773ad3a012beb66faf06d2f46057
ndandax <sebandandala99@gmail.com>
2023-06-14 12:03:17 +0300

    javascript

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-exports.nbOccurences = function (list, searchElement) {
+exports.nbOccurrences = function (list, searchElement) {
   let count = 0;
-  for (let i = 0; list.length < i; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       count++;
     }
