94a9ce0de801018b49c48231dbbc68fff8760778
ndandax <sebandandala99@gmail.com>
2023-06-14 12:10:07 +0300

    update

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
+/*
+Function returns number of occurences in a list
+*/
 'use strict';
-exports.nbOccurrences = function (list, searchElement) {
-  let count = 0;
+exports.nbOccurences = function (list, searchElement) {
+  let occurences = 0;
+
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
-      count++;
+      occurences += 1;
     }
   }
-  return count;
+  return (occurences);
 };
